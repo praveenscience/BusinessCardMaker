779f17445c6005ee2610c1b411231b672699e032
Praveen Kumar <praveenscience@gmail.com>
2020-10-25 14:53:16 +0000

    Created state variables and passed to Form.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Form from "./Form";
 import Preview from "./Preview";
 
 const App = () => {
+  const [Name, setName] = useState("");
+  const [Designation, setDesignation] = useState("");
+  const [Phone, setPhone] = useState("");
+  const [Email, setEmail] = useState("");
+  const Values = {
+    Name,
+    Designation,
+    Phone,
+    Email
+  };
   return (
     <div className="App">
       <Header Dark={true}>Professional Card Maker</Header>
       <main>
-        <Form />
+        <Form Values={Values} />
         <Preview />
       </main>
     </div>
