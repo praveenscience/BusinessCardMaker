2c32b80090d1984f1b577595d7ddddaa1f08fac2
Praveen Kumar <praveenscience@gmail.com>
2020-10-25 14:37:55 +0000

    Added both Form and Preview components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "./Header";
+import Form from "./Form";
+import Preview from "./Preview";
 
 const App = () => {
   return (
     <div className="App">
       <Header Dark={true}>Professional Card Maker</Header>
+      <main>
+        <Form />
+        <Preview />
+      </main>
     </div>
   );
 };
