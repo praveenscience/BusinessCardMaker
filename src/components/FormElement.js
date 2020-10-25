42873d93607469193c64c8fb51ef4d5171bb9414
Praveen Kumar <praveenscience@gmail.com>
2020-10-25 14:51:35 +0000

    Added dummy onChange function.

--- src/components/FormElement.js
+++ src/components/FormElement.js
@@ -1,10 +1,11 @@
 import React from "react";
 
+const dummyOnChange = () => {};
 const FormElement = ({ ID, Label, Value }) => {
   return (
     <label>
       <strong>{Label}</strong>
-      <input type="text" name={ID} value={Value} />
+      <input type="text" name={ID} value={Value} onChange={dummyOnChange} />
     </label>
   );
 };
