1bdfafa44c1efcdc1f569d667e9eb884770f76b3
Praveen Kumar <praveenscience@gmail.com>
2020-10-25 15:23:15 +0000

    Implemented Download of Business Card.

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { createRef } from "react";
+import {
+  exportComponentAsJPEG,
+  exportComponentAsPDF,
+  exportComponentAsPNG
+} from "react-component-export-image";
 import BusinessCard from "./BusinessCard";
 
 const Preview = ({ Values }) => {
+  const bcRef = createRef();
+  const handleDownload = e => {
+    e.preventDefault();
+    const downloadAs = {
+      exportComponentAsJPEG,
+      exportComponentAsPDF,
+      exportComponentAsPNG
+    };
+    downloadAs[
+      "exportComponentAs" + e.target.innerHTML.trim().replace("Save as ", "")
+    ](bcRef, Values.Name);
+  };
   return (
     <section className="Preview">
       <h2>Preview Pane</h2>
-      <div className="Preview-Wrapper">
+      <div className="Preview-Wrapper" ref={bcRef}>
         <BusinessCard Values={Values} />
       </div>
       <div className="Preview-Actions">
-        <button>Save as PDF</button>
-        <button>Save as PNG</button>
-        <button>Save as JPEG</button>
+        <button onClick={handleDownload}>Save as PDF</button>
+        <button onClick={handleDownload}>Save as PNG</button>
+        <button onClick={handleDownload}>Save as JPEG</button>
       </div>
     </section>
   );
