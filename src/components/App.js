72e35d099a9919fcdd7b94fa024eb15fb4f70624
Praveen Kumar <praveenscience@gmail.com>
2020-10-25 14:27:41 +0000

    Added Headers and styles in App.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 const App = () => {
-  return <div className="App">Hello Everyone!</div>;
+  return (
+    <div className="App">
+      <Header Dark={true}>Professional Card Maker</Header>
+    </div>
+  );
 };
 
 export default App;
