cbd80203825e51e0afc54ffa2e9f721a3bff6a60
Praveen Kumar <praveenscience@gmail.com>
2020-10-25 15:14:11 +0000

    Included Business Card component and styles.

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,9 +1,13 @@
 import React from "react";
+import BusinessCard from "./BusinessCard";
 
 const Preview = ({ Values }) => {
   return (
     <section className="Preview">
       <h2>Preview Pane</h2>
+      <div className="Preview-Wrapper">
+        <BusinessCard Values={Values} />
+      </div>
     </section>
   );
 };
