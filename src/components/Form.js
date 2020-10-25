4705ac5eb5db87b2d290cdf46ca9512d657ea37c
Praveen Kumar <praveenscience@gmail.com>
2020-10-25 14:53:00 +0000

    Implemented the values and fields from parent values.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,18 @@
 import React from "react";
 import FormElement from "./FormElement";
 
-const Form = () => {
+const Form = ({ onChange, Values }) => {
   return (
     <section className="Form">
       <h2>Enter your Details</h2>
-      <form>
-        {["Name", "Designation", "Phone", "Email"].map((field, key) => (
-          <FormElement ID={field} Label={field} key={key} />
+      <form onChange={onChange}>
+        {Object.keys(Values).map((field, key) => (
+          <FormElement
+            ID={field}
+            Label={field}
+            Value={Values[field]}
+            key={key}
+          />
         ))}
       </form>
     </section>
